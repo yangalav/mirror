1b2d57d88f1ed4990e52306e799ac76bf088cc07
Cameron <camdunne@gmail.com>
2017-06-23 22:08:08 -0400

    delete

--- servertest/config/routes.js
+++
@@ -1,8 +0,0 @@
-import { redirect, logout } from '../controllers/user';
-
-
-export default (app, passport) => {
-  app.post('/api/users/login', passport.authenticate('local-login'), redirect);
-  app.post('/api/users/register', passport.authenticate('local-signup'), redirect);
-  app.get('/api/users/logout', logout);
-};
--- servertest/controllers/user.js
+++
@@ -1,9 +0,0 @@
-
-export const redirect = (req, res) => {
-  res.send(req.user);
-};
-
-export const logout = (req, res) => {
-  req.logout();
-  return res.json({ success: true });
-};
